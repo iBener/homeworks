ad755a43400d5c36dd07a7a0ac6e8d43069347d9
İbrahim Bener <ibrahimbener@outlook.com>
2016-03-27 21:30:19 +0300

    geliştirme

---
+++ LOCCounterNet/LOCCounterNet/Proje.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO = System.IO;
+
+namespace LOCCounterNet
+{
+    class Proje
+    {
+        private string _anaPath;
+        private List<string> _dosyalar;
+
+        public Proje(string path)
+        {
+            _anaPath = path;
+            _dosyalar = new List<string>();
+            DosyalariOku(_anaPath);
+        }
+
+        private void DosyalariOku(string path)
+        {
+            if (IO.File.Exists(path))
+            {
+                _dosyalar.Add(path);
+                return;
+            }
+            if (IO.Directory.Exists(path))
+            {
+                foreach (var item in IO.Directory.GetFiles(path))
+                {
+                    _dosyalar.Add(item);
+                }
+                foreach (var item in IO.Directory.GetDirectories(path))
+                {
+                    DosyalariOku(item);
+                }
+            }
+        }
+
+        public IEnumerable<string> Dosyalar
+        {
+            get
+            {
+                foreach (var item in _dosyalar)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+    }
+}
---
+++ LOCCounterNet/LOCCounterNet/SatirSayici.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IO = System.IO;
+
+namespace LOCCounterNet
+{
+    class SatirSayici
+    {
+        List<IKaynakKod> KodSayicilar { get; set; }
+
+        public bool Olustur()
+        {
+            try
+            {
+                KodSayicilar = new List<IKaynakKod>();
+
+                var type = typeof(IKaynakKod);
+                var types = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(s => s.GetTypes())
+                    .Where(p => type.IsAssignableFrom(p));
+
+                foreach (var sayici in types)
+                {
+                    if (!sayici.IsInterface)
+                    {
+                        var ks = Activator.CreateInstance(sayici) as IKaynakKod;
+                        if (ks != null)
+                        {
+                            KodSayicilar.Add(ks);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+            return true;
+        }
+
+        public void Say(string[] args)
+        {
+            foreach (var path in args)
+            {
+                var p = new Proje(path);
+                foreach (var item in p.Dosyalar)
+                {
+                    foreach (var ks in KodSayicilar)
+                    {
+                        if (UygunKaynakKodu(item, ks))
+                        {
+                            var lines = IO.File.ReadAllLines(item);
+                            ks.SatirSay(lines);
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool UygunKaynakKodu(string item, IKaynakKod ks)
+        {
+            var dosyaUzantisi = IO.Path.GetExtension(item);
+            foreach (var uzanti in ks.DosyaFiltresi.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (dosyaUzantisi.Equals(uzanti))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
